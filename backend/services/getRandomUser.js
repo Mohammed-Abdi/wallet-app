2b6dce538a77bf0c3e11ec765e672ba1c0c1df45
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-12 10:07:06 +0300

    feat: add error handling incase it failed to fetch users

--- backend/services/getRandomUser.js
+++ backend/services/getRandomUser.js
@@ -1,17 +1,26 @@
 export async function getRandomUser() {
-  const res = await fetch("https://randomuser.me/api/");
-  const data = await res.json();
-  const result = data.results[0];
+  try {
+    const res = await fetch("https://randomuser.me/api/");
+    const data = await res.json();
+    if (!data?.results?.length) {
+      throw new Error("No results found in API response");
+    }
 
-  return {
-    name: `${result.name.first} ${result.name.last}`,
-    email: result.email,
-    age: result.dob.age,
-    gender: result.gender,
-    profilePicture: result.picture.large,
-    location: {
-      city: result.location.city,
-      country: result.location.country,
-    },
-  };
+    const result = data.results[0];
+
+    return {
+      name: `${result.name.first} ${result.name.last}`,
+      email: result.email,
+      age: result.dob.age,
+      gender: result.gender,
+      profilePicture: result.picture.large,
+      location: {
+        city: result.location.city,
+        country: result.location.country,
+      },
+    };
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
 }
