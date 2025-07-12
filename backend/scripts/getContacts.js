0eefcf6f6672e8d47e82faa19591723a98eb7f43
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-12 12:38:02 +0300

    test: check the issue by uncommenting the condition

--- backend/scripts/getContacts.js
+++ backend/scripts/getContacts.js
@@ -3,9 +3,9 @@ import { generateContacts } from "../scripts/generateContacts.js";
 export async function getContacts(numberOfContacts) {
   try {
     const contacts = await generateContacts(numberOfContacts);
-    // if (!Array.isArray(contacts) || contacts.length === 0) {
-    //   throw new Error("No contacts generated");
-    // }
+    if (!Array.isArray(contacts) || contacts.length === 0) {
+      throw new Error("No contacts generated");
+    }
     return "test: contacts fetched";
   } catch (error) {
     console.error("getContacts error:", error.message);
