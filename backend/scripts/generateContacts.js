235026f2c501f5f6fb915e3c4d0aa870481e0986
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-12 10:09:09 +0300

    feat: add contact generator to get random contacts

---
+++ backend/scripts/generateContacts.js
@@ -0,0 +1,24 @@
+import { getRandomUser } from "../services/getRandomUser.js";
+
+export async function generateContacts(count = 1) {
+  try {
+    const contacts = await Promise.all(
+      [...Array(count)].map(async () => {
+        const randomUser = await getRandomUser();
+        return randomUser;
+      })
+    );
+
+    return contacts.filter(Boolean).map((user) => ({
+      name: user.name,
+      email: user.email,
+      age: user.age,
+      gender: user.gender,
+      profilePicture: user.profilePicture,
+      location: user.location,
+    }));
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+}
