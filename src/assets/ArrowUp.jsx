1450d4b564014cf77c660af298f3b6f1e12a4e22
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-13 12:22:11 +0300

    chore: add arrow icon for activity history

---
+++ src/assets/ArrowUp.jsx
@@ -0,0 +1,20 @@
+export default function ArrowUp(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={48}
+      height={48}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M12 5v14m6-8l-6-6m-6 6l6-6"
+      ></path>
+    </svg>
+  );
+}
