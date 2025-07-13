8a7c7579a5a386e3b43dbd9d8eea742373a27112
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-13 09:34:41 +0300

    feat: add classname for dynamic style change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,22 @@ import { ThemeContext } from "../context/ThemeContext";
 
 const cardStyle = {
   display: "flex",
+  flexDirection: "column",
+  gap: "0.75rem",
+  fontSize: "0.875rem",
   alignItems: "center",
   justifyContent: "center",
   width: "6.25rem",
   height: "6.25rem",
   borderRadius: "0.5rem",
+  cursor: "pointer",
 };
 
 function Card({ children }) {
   const { theme } = useContext(ThemeContext);
   return (
     <div
+      className="card"
       style={{
         ...cardStyle,
         backgroundColor: `var(--${theme}-bright-clr)`,
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -58,7 +58,10 @@ function Dashboard() {
         </p>
       </div>
       <div>
-        <Card>Deposit</Card>
+        <Card>
+          <span>💰</span>
+          <p>Deposit</p>
+        </Card>
       </div>
     </main>
   );
