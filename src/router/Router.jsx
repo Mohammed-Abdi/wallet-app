c979c86b006cad764bf429ef42b7a7f338346a18
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-16 16:51:40 +0300

    feat: enhance routing of dashboard to only work if there is active user

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,13 +4,26 @@ import Dashboard from "../pages/dashboard/Dashboard";
 import Signup from "../pages/signup/Signup";
 import Login from "../pages/login/Login";
 import NotFound from "../pages/not-found/NotFound";
+import { useContext, useMemo } from "react";
+import { AccountContext } from "../context/AccountContext";
 
 function Router() {
+  const { accounts } = useContext(AccountContext);
+
+  const userIsActive = useMemo(() => {
+    return accounts.some(
+      (account) => account.status.accountStatus === "active"
+    );
+  }, [accounts]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={userIsActive ? <Dashboard /> : <NotFound />}
+      />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
