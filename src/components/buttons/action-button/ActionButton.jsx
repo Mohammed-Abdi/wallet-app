727857c9a6f40bc9998552d9f9ca4aa68649d7b1
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-17 11:46:24 +0300

    feat: add ref attribute for auto focusing on the button

--- src/components/buttons/action-button/ActionButton.jsx
+++ src/components/buttons/action-button/ActionButton.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
 import styles from "./ActionButton.module.css";
 
-function ActionButton({ children, style, onClick }) {
+function ActionButton({ children, style, onClick, ref }) {
   const { theme } = useContext(ThemeContext);
 
   return (
     <button
+      ref={ref}
       onClick={onClick}
       className={`${styles.button} ${
         theme === "dark" ? styles.dark : theme === "light" ? styles.light : ""
