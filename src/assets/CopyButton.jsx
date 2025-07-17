b1ce6a61746257bf689a4f64bf2c7793cd953842
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-17 08:36:54 +0300

    chore: add copy and paste buttons

---
+++ src/assets/CopyButton.jsx
@@ -0,0 +1,21 @@
+export default function CopyButton({ width = 24, height = 24, style }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      style={{ ...style }}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={1.5}
+        d="M6 15h-.6C4.07 15 3 13.93 3 12.6V5.4C3 4.07 4.07 3 5.4 3h7.2C13.93 3 15 4.07 15 5.4V6m-3.6 3h7.2a2.4 2.4 0 0 1 2.4 2.4v7.2a2.4 2.4 0 0 1-2.4 2.4h-7.2A2.4 2.4 0 0 1 9 18.6v-7.2A2.4 2.4 0 0 1 11.4 9"
+      ></path>
+    </svg>
+  );
+}
