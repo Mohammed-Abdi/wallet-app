c8be069f2f212d99d6eeae2b972091619436d7de
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-07-19 10:01:47 +0300

    refactor: remove hero animation for a cleaner and more modern UI

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,6 @@ import styles from "./Hero.module.css";
 import Highlight from "../Highlight";
 import ActionButton from "../buttons/action-button/ActionButton";
 import SecondaryButton from "../buttons/secondary-button/SecondaryButton";
-import WalletAnimation from "../../assets/WalletAnimation";
 import { Link, useNavigate } from "react-router-dom";
 
 function Hero() {
@@ -13,33 +12,28 @@ function Hero() {
 
   return (
     <section className={styles.hero}>
-      <div className={styles.left}>
-        <Highlight fontSize="0.875rem">
-          <p>Simple. Secure. Personal →</p>
-        </Highlight>
-        <h1 style={{ width: "min(750px, 100%)", fontWeight: 500 }}>
-          The smarter, simpler way to launch, manage, and personalize your
-          digital wallet experience.
-        </h1>
-        <div className={styles.buttons}>
-          <Link to="/signup">
-            <ActionButton style={{ padding: "0.75rem 2rem" }}>
-              <p>Get Started →</p>
-            </ActionButton>
-          </Link>
-          <SecondaryButton
-            style={{ padding: "0.75rem 2rem" }}
-            onClick={() => {
-              accountDispatch({ type: "login", payload: "guest_user" });
-              navigate("/dashboard");
-            }}
-          >
-            <p>Demo Account</p>
-          </SecondaryButton>
-        </div>
-      </div>
-      <div className={styles.right}>
-        <WalletAnimation />
+      <Highlight fontSize="0.875rem">
+        <p>Simple. Secure. Personal →</p>
+      </Highlight>
+      <h1 style={{ width: "min(750px, 100%)", fontWeight: 500 }}>
+        The smarter, simpler way to launch, manage, and personalize your digital
+        wallet experience.
+      </h1>
+      <div className={styles.buttons}>
+        <Link to="/signup">
+          <ActionButton style={{ padding: "0.75rem 2rem" }}>
+            <p>Get Started →</p>
+          </ActionButton>
+        </Link>
+        <SecondaryButton
+          style={{ padding: "0.75rem 2rem" }}
+          onClick={() => {
+            accountDispatch({ type: "login", payload: "guest_user" });
+            navigate("/dashboard");
+          }}
+        >
+          <p>Demo Account</p>
+        </SecondaryButton>
       </div>
     </section>
   );
